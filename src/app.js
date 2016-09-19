cccf640ac8ea60d41e08a33a9adffa43b0742794
cavossman <cvossman@ksu.edu>
2016-09-19 15:24:46 -0500

    in class work
    Couldn't run browserify in class.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ var game = new Game(canvas, update, render);
 // We have 9 pairs of possible cards that are about 212px square
 var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 var board = [];
+while(cards.length > 0) {
+	var index = Math.floor(Math.random() * (cards.length - 1));
+	board.push({card: cards[index], flip: false});
+	cards.splice(index, 1);
+}
+console.log(board); //to see the board contents
 
 // TODO: Place the cards on the board in random order
 
@@ -55,4 +61,34 @@ function render(elapsedTime, ctx) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // TODO: Render the board
+  var x = 0;
+  var y = 0;
+  /*for (int i = 0; i < board.length; i++){
+	  ctx.fillStyle = "#000000";
+	  ctx.fillRect = (x, y, 212, 212);
+	  if (x + 212 >= canvas.width){
+		  x = 0;
+		  y += 212;
+	  }
+	  else {
+		  x+= 212;
+	  }
+  }*/
+  for(var y = 0; y < 3; y++){
+	  for (var x = 0; x < 6; x++){
+		  var card = board[y * 6 + x];
+		  if(card.flip){
+			  ctx.drawImage(image,
+			  // source - fix to pic different pictures from source.
+			  0, 0, 212, 212, 
+			  // destination
+			  x * 165 + 3, y * 165 + 3, 160, 160
+			  )
+		  }
+		  else {
+			  ctx.fillStyle = "#3333ff";
+			  ctz.fillRect(x * 165 + 3, y * 165 + 3, 160, 160); // 5 pixel padding
+		  }
+	  }
+  }
 }
