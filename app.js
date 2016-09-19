7b27ad20ac742fb8a05aa4909f4bad3fdd8f60d3
cavossman <cvossman@ksu.edu>
2016-09-19 15:24:08 -0500

    in class work
    couldn't run browserify in class to update the bundle.

---
+++ app.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/* Classes */
+const Game = require('./game');
+
+/* Global variables */
+var canvas = document.getElementById('screen');
+var game = new Game(canvas, update, render);
+
+// We have 9 pairs of possible cards that are about 212px square
+var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+var board = [];
+while(cards.length > 0) {
+	var index = Math.floor(Math.random() * (cards.length - 1));
+	board.push({card: cards[index], flip: false});
+	cards.splice(index, 1);
+}
+console.log(board); //to see the board contents
+
+// TODO: Place the cards on the board in random order
+
+canvas.onclick = function(event) {
+  event.preventDefault();
+  // TODO: determine which card was clicked on
+  // TODO: determine what to do
+}
+
+/**
+ * @function masterLoop
+ * Advances the game in sync with the refresh rate of the screen
+ * @param {DOMHighResTimeStamp} timestamp the current time
+ */
+var masterLoop = function(timestamp) {
+  game.loop(timestamp);
+  window.requestAnimationFrame(masterLoop);
+}
+masterLoop(performance.now());
+
+
+/**
+ * @function update
+ * Updates the game state, moving
+ * game objects and handling interactions
+ * between them.
+ * @param {DOMHighResTimeStamp} elapsedTime indicates
+ * the number of milliseconds passed since the last frame.
+ */
+function update(elapsedTime) {
+
+}
+
+/**
+  * @function render
+  * Renders the current game state into a back buffer.
+  * @param {DOMHighResTimeStamp} elapsedTime indicates
+  * the number of milliseconds passed since the last frame.
+  * @param {CanvasRenderingContext2D} ctx the context to render to
+  */
+function render(elapsedTime, ctx) {
+  ctx.fillStyle = "#ff7777";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // TODO: Render the board
+  var x = 0;
+  var y = 0;
+  /*for (int i = 0; i < board.length; i++){
+	  ctx.fillStyle = "#000000";
+	  ctx.fillRect = (x, y, 212, 212);
+	  if (x + 212 >= canvas.width){
+		  x = 0;
+		  y += 212;
+	  }
+	  else {
+		  x+= 212;
+	  }
+  }*/
+  for(var y = 0; y < 3; y++){
+	  for (var x = 0; x < 6; x++){
+		  var card = board[y * 6 + x];
+		  if(card.flip){
+			  ctx.drawImage(image,
+			  // source - fix to pic different pictures from source.
+			  0, 0, 212, 212, 
+			  // destination
+			  x * 165 + 3, y * 165 + 3, 160, 160
+			  )
+		  }
+		  else {
+			  ctx.fillStyle = "#3333ff";
+			  ctz.fillRect(x * 165 + 3, y * 165 + 3, 160, 160); // 5 pixel padding
+		  }
+	  }
+  }
+}
